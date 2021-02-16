3d7c343e1bef2633be51b8d139ae3f64f28da8fb
Jon Yocky <6563138+Curlyfries13@users.noreply.github.com>
2021-02-15 23:12:01 -0700

    Remove dependency on Reactstrap
    * implement dropdown transition using styled components and transitions
    * remove imports to unused modules in Header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,48 +1,127 @@
-import React, { useState } from 'react';
-import { Link, NavLink as RouterNavLink} from 'react-router-dom';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink as RouterNavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
-// import { NavLink } from 'react-router-dom';
-import AboutPage from '../about/About';
 import brandLogo from '../../assets/g-damage-logo-2.svg';
 
-const Header = () => {
-  const activeClassName = 'active';
-  const [isOpen, setIsOpen] = useState(false);
+const NavContainer = styled.div`
+  flex-wrap: ${(props: { collapse: boolean }) => {
+    return props.collapse ? 'wrap' : 'nowrap';
+  }};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
 
-  const toggle = () => setIsOpen(!isOpen);
+// TODO: In here I'm hard-coding the dropdown height: I'm not a fan
+const NavCollapse = styled.div`
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-height: ${(props: { navDropdown: boolean; height: string }) => {
+    if (props.navDropdown) {
+      return '120px';
+    } else {
+      return '0px';
+    }
+  }};
+  overflow: hidden;
+  transition: max-height 0.5s ease;
+`;
+
+const NavFlat = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const NavList = styled.ul`
+  flex-direction: ${(props: { collapse: boolean }) => {
+    return props.collapse ? 'column' : 'row';
+  }};
+`;
+
+const Header = () => {
+  const [collapse, setCollapse] = useState(false);
+  const [navDropdown, setNavDropdown] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 750) {
+        setCollapse(true);
+        setNavDropdown(false);
+      } else {
+        setCollapse(false);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const ListItems = () => {
+    return (
+      <NavList className='navbar-nav' collapse={collapse}>
+        <li className='nav-item'>
+          <RouterNavLink
+            className='nav-link px-2'
+            activeClassName='active'
+            to='/'
+            exact
+          >
+            Home
+          </RouterNavLink>
+        </li>
+        <li className='nav-item'>
+          <RouterNavLink
+            className='nav-link px-2'
+            activeClassName='active'
+            to='/calc'
+          >
+            Calc
+          </RouterNavLink>
+        </li>
+        <li className='nav-item'>
+          <RouterNavLink
+            className='nav-link px-2'
+            activeClassName='active'
+            to='/about'
+          >
+            About
+          </RouterNavLink>
+        </li>
+      </NavList>
+    );
+  };
 
   return (
-    <Navbar color='primary' dark expand='md' className='navbar'>
-      <div className='container-fluid'>
-        <NavbarBrand to='/'>
+    <nav className='navbar navbar-dark bg-primary'>
+      <NavContainer className='px-3' collapse={collapse}>
+        <a className='navbar-brand' href='/'>
           <img src={brandLogo} min-width='50' alt='g-damage-calc logo' />
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} className='navbar-collapse' navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink tag={RouterNavLink} to="/"
-              activeClassName="active" exact>Home</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink tag={RouterNavLink} to="/calc" activeClassName="active">Calc</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink tag={RouterNavLink} to="/about" activeClassName="active">About</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </div>
-    </Navbar>
+        </a>
+        {collapse ? (
+          <>
+            <button
+              className='btn navbar-toggler'
+              onClick={() => setNavDropdown(!navDropdown)}
+            >
+              <i className='bi bi-list'></i>
+            </button>
+            <NavCollapse navDropdown={navDropdown}>
+              <ListItems />
+            </NavCollapse>
+          </>
+        ) : (
+          <NavFlat>
+            <ListItems />
+          </NavFlat>
+        )}
+      </NavContainer>
+    </nav>
   );
 };
 
