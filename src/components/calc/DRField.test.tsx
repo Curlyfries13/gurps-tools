0322851784c9fd578d7dfd6521a478d83caaa6cb
Jon Yocky <6563138+Curlyfries13@users.noreply.github.com>
2021-03-06 22:36:07 -0700

    Add unit test for DR validation messages

--- src/components/calc/DRField.test.tsx
+++ src/components/calc/DRField.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { RenderResult } from '@testing-library/react';
 import DRField from './DRField';
 
-import { render } from 'tools/test-utils';
+import { render, fireEvent, screen } from 'tools/test-utils';
 import store from 'src/redux';
 
 const mockArmor = { dr: 0, id: 0 };
@@ -14,4 +14,15 @@ it('matches snapshot', async () => {
     initialState: mockState,
   });
   expect(component).toMatchSnapshot();
+});
+
+it('displays an error message on invalid DR', async () => {
+  const component: RenderResult = render(<DRField armorId={0} />, {
+    initialState: mockState,
+  });
+  fireEvent.change(screen.getByLabelText(/Summary Damage Resistence/i), {
+    target: { value: 'a' },
+  });
+  const test = await screen.findByText(/DR must be a non-negative value/);
+  expect(test).not.toBe(null);
 });
