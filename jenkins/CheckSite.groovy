c58d52afaa4114ce95299333785226e6211d2f60
Jon Yocky <6563138+Curlyfries13@users.noreply.github.com>
2021-03-14 18:58:50 -0700

    Add check groovy script for automated deploy
    * add check script; this runs build then deploy. Configuration required
    on jenkins server for polling interval and credentials.

---
+++ jenkins/CheckSite.groovy
@@ -0,0 +1,41 @@
+/* A pipeline to determine if hugo has any pages to publish
+Notes:
+only checks the master branch - development branches are not monitored
+*/
+pipeline {
+  agent any
+  options {
+    timeout(time: 15, unit: "MINUTES")
+    disableConcurrentBuilds()
+    buildDiscarder(
+      logRotator(
+        artifactDaysToKeepStr: "",
+        artifactNumToKeepStr: "1",
+        daysToKeepStr: "",
+        numToKeepStr: "10")
+    )
+  }
+  parameters{
+    string(name: 'branch', defaultValue: 'master')
+    string(name: 'gitCredentials')
+    string(name: 'firebaseToken')
+  }
+  environment {
+    res = false;
+  }
+  stages {
+    stage("request redeploy") {
+      steps{
+        script {
+          build job: 'build-gurps-tools', parameters:[
+            string(name: 'branch', value: params.branch),
+            string(name: 'gitCredentials' value: params.gitCredentials)
+          ]
+          build job: 'deploy-gurps-tools', parameters:[
+            string(name:'firebaseToken', value: params.firebaseToken)
+          ]
+        }
+      }
+    }
+  }
+}
